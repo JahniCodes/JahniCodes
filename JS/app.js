44d5744502ba857b7d3f73321e07c4bfa7b68d02
Jahni Anderson <160801626+JahniCodes@users.noreply.github.com>
2024-03-08 12:52:55 -0500

    add content to all files

---
+++ JS/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the current page URL
+    var currentPage = window.location.pathname.split("/").pop();
+    console.log(currentPage)
+
+  // Query all navbar links
+  var links = document.querySelectorAll(".navbar-nav .navbar-link");
+
+  // Iterate through each link to find the active one
+  links.forEach(function (link) {
+    if (link.getAttribute("href") === currentPage) {
+      // Add 'active' class to the parent '.navbar-link' of the SVG
+      link.classList.add("active");
+    }
+  });
+});
